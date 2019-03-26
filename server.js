0b9b27fecebbc7247110a87485726cb0554726d9
Feilong Huang <felixh6888@gmail.com>
2019-03-25 20:45:43 -0400

    new

---
+++ server.js
@@ -0,0 +1,6 @@
+const sequelize = "sequelize";
+const express = require("express");
+const routes = require("./routes");
+//const routes = require("./routes/api-routes");
+const session = require("express-session");
+const path = require("path");
